90caf9e57f4ef0078ab8291992cdb9d3b52f9939
Wenxian-ee <wenxianwzf@163.com>
2025-05-22 23:05:58 +0800

    Zhifei Reconstruct the integration branch

--- ledgerAPP3.7/ledgerAPP/src/main/java/com/shelton/ebu6403/controller/SettingController.java
+++
@@ -1,4 +0,0 @@
-package com.shelton.ebu6403.controller;
-
-public class SettingController {
-}
---
+++ src/main/java/com/shelton/ebu6403/controller/SettingController.java
@@ -0,0 +1,4 @@
+package main.java.com.shelton.ebu6403.controller;
+
+public class SettingController {
+}
